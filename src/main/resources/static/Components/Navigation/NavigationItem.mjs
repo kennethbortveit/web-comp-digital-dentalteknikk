eebd4f085fb5b0719b14ec77d3ec9f3e2e615413
Kenneth Haugen <kenneth.bortveit@gmail.com>
2024-12-27 17:07:05 +0100

    Refactor nav-item css.

--- src/main/resources/static/Components/Navigation/NavigationItem.mjs
+++ src/main/resources/static/Components/Navigation/NavigationItem.mjs
@@ -2,13 +2,23 @@ import DDComponent from '../DDComponent.mjs'
 
 export default class NavigationItem extends DDComponent
 {
-	#stylesheetUrl = './Components/Navigation/NavigationItem.css'
+	static styles = `
+		div {
+			color: var(--grey);
+			padding: var(--spacing-small);
+		}
+		div:hover {
+			cursor: pointer;
+			color: var(--white);
+			background-color: var(--blue);
+		}
+	`
 	constructor() {
 		super()
 	}
 
 	connectedCallback() {
-		this.appendExternalStyleSheet(this.#stylesheetUrl)
+		this.applyStyles(NavigationItem.styles)
 		const container = this.#createContainer()
 		const text = this.#createText()
 		container.appendChild(text)
